3fcd381002300558991a00e760f87a43a7e80df5
Lexsak <bartoszpiprek123@gmail.com>
2023-12-01 21:20:07 +0100

    to-do list first commit

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.css']
+  styleUrls: ['./task-list.component.css'],
 })
 export class TaskListComponent {
+  tasks: string[] = [];
+  newTask: string = '';
 
+  constructor(private taskService: TaskService) {
+    this.tasks = this.taskService.getTasks();
+  }
+
+  addTask(): void {
+    if (this.newTask.trim() !== '') {
+      this.taskService.addTask(this.newTask.trim());
+      this.newTask = '';
+      this.tasks = this.taskService.getTasks();
+    }
+  }
+
+  removeTask(index: number): void {
+    this.taskService.removeTask(index);
+    this.tasks = this.taskService.getTasks();
+  }
+
+  markAsCompleted(index: number): void {
+    this.taskService.markTaskAsCompleted(index);
+    this.tasks = this.taskService.getTasks();
+  }
 }
--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  private tasks: string[] = [];
 
-  constructor() { }
+  getTasks(): string[] {
+    return this.tasks;
+  }
+
+  addTask(task: string): void {
+    this.tasks.push(task);
+  }
+
+  removeTask(index: number): void {
+    if (index >= 0 && index < this.tasks.length) {
+      this.tasks.splice(index, 1);
+    }
+  }
+
+  markTaskAsCompleted(index: number): void {
+    if (index >= 0 && index < this.tasks.length) {
+      // You can implement additional logic here if needed
+      console.log(`Task "${this.tasks[index]}" marked as completed.`);
+    }
+  }
 }
