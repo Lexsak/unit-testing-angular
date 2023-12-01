4942dcf85e03fa8e1d6f7a19ca74033547b699ed
Lexsak <bartoszpiprek123@gmail.com>
2023-12-01 21:57:18 +0100

    task-list unit test

--- src/app/task-list/task-list.component.spec.ts
+++ src/app/task-list/task-list.component.spec.ts
@@ -1,21 +1,45 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { TaskListComponent } from './task-list.component';
+import { TaskService } from '../task.service';
 
-// import { TaskListComponent } from './task-list.component';
+describe('TaskListComponent', () => {
+  let component: TaskListComponent;
+  let fixture: ComponentFixture<TaskListComponent>;
 
-// describe('TaskListComponent', () => {
-//   let component: TaskListComponent;
-//   let fixture: ComponentFixture<TaskListComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TaskListComponent],
+      imports: [FormsModule],
+      providers: [TaskService],
+    });
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [TaskListComponent]
-//     });
-//     fixture = TestBed.createComponent(TaskListComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(TaskListComponent);
+    component = fixture.componentInstance;
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should add task', () => {
+    const task = 'Test Task';
+    component.newTask = task;
+    component.addTask();
+    expect(component.tasks.some((t) => t.text === task)).toBeTruthy();
+  });
+
+  it('should remove task', () => {
+    const task = 'Test Task';
+    component.tasks = [{ text: task, completed: false }];
+    component.removeTask(0);
+    expect(component.tasks.length).toBe(0);
+  });
+
+  it('should mark task as completed', () => {
+    const task = 'Test Task';
+    component.tasks = [{ text: task, completed: false }];
+    component.markAsCompleted(0);
+    expect(component.tasks[0].completed).toBeTruthy();
+  });
+});
