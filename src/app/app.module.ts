3243d41aedaf6d42653829a86e36f52c7b5426b0
Lexsak <bartoszpiprek123@gmail.com>
2023-12-02 18:58:46 +0100

    parent & child component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { FormsModule } from '@angular/forms';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskListComponent
+    TaskListComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
