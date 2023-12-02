a797446df1f223a787bb4282a4af240e75fe1186
Lexsak <bartoszpiprek123@gmail.com>
2023-12-02 19:25:21 +0100

    parent interval

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.css']
+  styleUrls: ['./parent.component.css'],
 })
-export class ParentComponent {
-  message = "Hello";
+export class ParentComponent implements OnInit {
+  message = 'Hello';
   number = 1;
+
+  ngOnInit(): void {
+    setInterval(() => {
+      ++this.number;
+    }, 1000);
+  }
 }
